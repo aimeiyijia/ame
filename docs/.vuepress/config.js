1c9325712f64f42774dea0ee38248da6033985c3
aimeiyijia <1456723200@qq.com>
2020-05-27 18:06:36 +0800

    增加方法部分

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -198,8 +198,8 @@ module.exports = {
 					clientID: "a6e1355287947096b88b",
 					clientSecret: "f0e77d070fabfcd5af95bebb82b2d574d7248d71",
 					repo: "vuepress-theme-vdoing", // GitHub 仓库
-					owner: "xugaoyi", // GitHub仓库所有者
-					admin: ["xugaoyi"], // 对仓库有写权限的人
+					owner: "aimeiyijia", // GitHub仓库所有者
+					admin: ["aimeiyijia"], // 对仓库有写权限的人
 					// distractionFreeMode: true,
 					pagerDirection: "last", // 'first'正序 | 'last'倒序
 					id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
