759d99ec455782ba72f58479ca05e50dc13943db
aimeiyijia <1456723200@qq.com>
2020-06-22 17:11:45 +0800

    更新下网站的关键词

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
 			{
 				name: "keywords",
 				content:
-					"前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown",
+					"前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown,vis-network中文",
 			},
 		],
 		["meta", { name: "baidu-site-verification", content: "7F55weZDDc" }], // 百度统计博主验证
