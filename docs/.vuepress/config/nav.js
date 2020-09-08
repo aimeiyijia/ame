39b69ef2ab8cabcbfa1ad1faf4857800b1884ce4
aimeiyijia <1456723200@qq.com>
2020-09-08 10:39:36 +0800

    修复无法正常打包

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -24,14 +24,13 @@ module.exports = [
 			},
 			{
 				text: "学习笔记",
-				items: [],
+				items: [{ text: "暂时为空", link: "" }],
 			},
 		],
 	},
 	{
 		text: "UI",
 		link: "/ui/",
-		items: [],
 	},
 	{
 		text: "计算机技术",
@@ -49,12 +48,7 @@ module.exports = [
 	{
 		text: "更多",
 		link: "/more/",
-		items: [
-			{ text: "学习", link: "/pages/a8692ab3bdcb4588/" },
-			{ text: "面试", link: "/pages/aea6571b7a8bae86/" },
-			{ text: "心情杂货", link: "/pages/2d615df9a36a98ed/" },
-			{ text: "友情链接", link: "/friends/" },
-		],
+		items: [{ text: "面试", link: "/pages/aea6571b7a8bae86/" }],
 	},
 	{ text: "关于", link: "/about/" },
 	{
