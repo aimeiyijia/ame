b76ad6cda5e54d9895bee0add02b262a5f5ed239
aimeiyijia <1456723200@qq.com>
2021-06-14 23:25:31 +0800

    修复自动部署bug

--- utils/autoDeployment.js
+++ utils/autoDeployment.js
@@ -1,35 +1,37 @@
-const fs = require("fs"),
-	path = require("path"),
-	process = require("process"),
-	NodeSSH = require("node-ssh"),
-	chalk = require("chalk");
-const ssh = new NodeSSH();
-const rootPath = path.join(__dirname, "..");
-const storageDirectoryInLocal = path.join(rootPath, "/docs/.vuepress/dist");
-const storageDirectoryInRemote = "/home/test";
+const fs = require('fs'),
+  path = require('path'),
+  process = require('process'),
+  { NodeSSH } = require('node-ssh'),
+  chalk = require('chalk')
+const ssh = new NodeSSH()
+const rootPath = path.join(__dirname, '..')
+const storageDirectoryInLocal = path.join(rootPath, '/docs/.vuepress/dist')
+const storageDirectoryInRemote = '/home/ame'
 
-ssh.connect({
-	host: "121.196.41.45",
-	username: "root",
-	// password: "abc123$%^",
-	port: 22, //SSH连接默认在22端口
-	privateKey: rootPath + "/utils/modules/id_rsa",
-})
-	.then(function(e) {
-		// Local, Remote
-		ssh.putDirectory(storageDirectoryInLocal, storageDirectoryInRemote)
-			.then(
-				function(Contents) {
-					console.log("文件夹内容传输成功！");
-				},
-				function(error) {
-					console.log(error, "文件夹内容传输失败！");
-				}
-			)
-			.finally(() => {
-				process.exit();
-			});
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+ssh
+  .connect({
+    host: '121.196.41.45',
+    username: 'root',
+    // password: "Liuxiaofan199445",
+    port: 22, //SSH连接默认在22端口
+    privateKey: rootPath + '/utils/modules/id_rsa',
+  })
+  .then(function(e) {
+    // Local, Remote
+    ssh
+      .putDirectory(storageDirectoryInLocal, storageDirectoryInRemote)
+      .then(
+        function(Contents) {
+          console.log('文件夹内容传输成功！')
+        },
+        function(error) {
+          console.log(error, '文件夹内容传输失败！')
+        }
+      )
+      .finally(() => {
+        process.exit()
+      })
+  })
+  .catch(err => {
+    console.log(err)
+  })
