94797367524158c98d1e85bb68afea13001d1aa5
aimeiyijia <1456723200@qq.com>
2021-07-24 23:44:23 +0800

    调整el-table-ts文档

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,12 +3,14 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
 import ElTableTs from "el-table-ts"
+import 'el-table-ts/dist/el-table-ts.css'
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
   siteData // 站点元数据
 }) => {
+  console.log(ElTableTs)
   Vue.use(ElementUI);
   Vue.use(ElTableTs)
 }
