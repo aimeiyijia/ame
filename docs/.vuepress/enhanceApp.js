1cf7c82756f1c331e8356037778fa1f7ca7faf11
aimeiyijia <1456723200@qq.com>
2021-09-20 21:07:35 +0800

    写el-form-plus文档

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,6 +5,8 @@ import "element-ui/lib/theme-chalk/index.css";
 
 import ElTableTs from "el-table-ts"
 import 'el-table-ts/dist/el-table-ts.css'
+
+import ElFormPlus from "el-form-plus"
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
@@ -14,4 +16,5 @@ export default ({
   console.log(ElTableTs)
   Vue.use(ElementUI);
   Vue.use(ElTableTs)
+  Vue.use(ElFormPlus)
 }
