7a36a4ca5fbdcbfd9db60f3733bf6e37112844d6
aimeiyijia <1456723200@qq.com>
2022-02-14 15:44:21 +0800

    feat：增加el-lists组件文档，并调整el-table-ts文档

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -6,6 +6,9 @@ import "element-ui/lib/theme-chalk/index.css";
 import ElTableTs from "el-table-ts"
 import 'el-table-ts/dist/el-table-ts.css'
 
+import ElLists from "el-lists"
+import 'el-lists/dist/el-lists.css'
+
 import ElFormPlus from "el-form-plus"
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
@@ -13,8 +16,8 @@ export default ({
   router, // 当前应用的路由实例
   siteData // 站点元数据
 }) => {
-  console.log(ElTableTs)
   Vue.use(ElementUI);
   Vue.use(ElTableTs)
+  Vue.use(ElLists)
   Vue.use(ElFormPlus)
 }
